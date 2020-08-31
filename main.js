f44d891bdcb116d00652c6989bf98f5ac4ba0627
zhihaofans <zhihaofans@hotmail.com>
2020-08-31 16:36:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
-
- $ui.render("scripts/main");
+var text = $context.text;
+var link = $context.link;
+var image = $context.image;
+if (text) {
+    $ui.alert({
+        title: "text",
+        message: text,
+        actions: [
+            {
+                title: "OK",
+                disabled: false, // Optional
+                handler: function() {}
+            }
+        ]
+    });
+} else if (link) {
+    $ui.alert({
+        title: "link",
+        message: link,
+        actions: [
+            {
+                title: "OK",
+                disabled: false, // Optional
+                handler: function() {}
+            }
+        ]
+    });
+} else if (image) {
+    $quicklook.open({
+        image: image
+    });
+} else {
+    $ui.render("scripts/main");
+}
