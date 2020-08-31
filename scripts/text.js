e6de2846556d4d1cf5c921217eabfc467d543c3f
zhihaofans <zhihaofans@hotmail.com>
2020-09-01 07:31:52 +0800

    Update text.js

--- scripts/text.js
+++ scripts/text.js
@@ -1,3 +1,37 @@
+let baidu = require("./sites/baidu");
+let searchEngine = [
+    {
+        id: "baidu_text",
+        title: "百度搜索"
+    }
+];
+async function askToDo(inputText) {
+    const menuResult = await $ui.menu(["搜索", "分词"]);
+    $console.info(menuResult);
+    //menuResult.index , menuResult.title
+    switch (menuResult) {
+        case 0:
+            search(inputText);
+            break;
+        default:
+            $ui.alert({
+                title: "错误",
+                message: "暂不支持该功能",
+                actions: [
+                    {
+                        title: "OK",
+                        disabled: false, // Optional
+                        handler: function() {}
+                    }
+                ]
+            });
+    }
+}
+async function search(inputText) {
+    const searchMenuResult = await $ui.menu(searchEngine.map(s => s.title));
+    
+}
 module.exports = {
-    baiduText: require("./sites/baidu").text
+    baiduText: baidu.text,
+    askToDo
 };
